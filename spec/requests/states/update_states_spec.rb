532ace64f59f62928f9c9aea0e8b02395582b8bf
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-04-02 12:10:17 -0700

    Get passing test for update_parks_spec

---
+++ spec/requests/states/update_states_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "get all states route", :type => :request do
+  
+  before do
+    @state = FactoryBot.create(:state)
+    put "/states/#{@state.id}", params: { name: "Of Mind" }
+  end
+
+  it 'returns updated name' do
+    expect(State.find(@state.id).name).to eq('Of Mind')
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
